861934807aed553be3085323029f4b488e7061f0
ikhfri <ikhsanfahrigultom@gmail.com>
2025-04-11 22:34:40 -0500

    1

--- src/validation/candidateSchema.ts
+++ src/validation/candidateSchema.ts
@@ -4,7 +4,7 @@ const candiateSchema = z.object({
   name: z.string().min(1, "Name is Required"),
   vision: z.string().min(1, "Vision is Required"),
   mission: z.string().min(1, "Mission is Required"),
-  image: z.instanceof(File).optional(),
+  image: z.any().optional(),
 });
 
 export default candiateSchema;
